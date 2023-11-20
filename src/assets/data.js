88dc705781155fe3ce5a90ec8792cb883277b320
HaytamElbadoui <elbadouihaytam219@gmail.com>
2023-11-20 08:48:00 +0100

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,15 @@
+const traductions = {
+  FR: {
+    title: "Bonjour et bienvenue sur mon site.",
+  },
+  EN: {
+    title: "Hello and welcome to my site.",
+   
+  },
+  ES: {
+    title: "Hola y bienvenido mi sitio.",
+
+  },
+};
+
+export default traductions;
